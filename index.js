0a3e960fab9840a52e1e9b41233cc27179a2f670
Wade Foster <>
2024-08-17 11:10:43 +1000

    `

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const body = document.querySelector("body");
 const mainDiv = document.createElement("div");
 mainDiv.setAttribute('id', 'mainDiv');
+let pixelCount = 256;
+let pixelWidth = 10;
+let pixelHeight = 10;
+
+function populatePixelsMainDiv() {
+    for (let i = 0; i < pixelCount; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        mainDiv.appendChild(pixel);
+    }
+}
 
 body.appendChild(mainDiv);
+
+populatePixelsMainDiv();
